211d1020592be2b55f47c9142697d54eea35e8ad
lucas-bernardino <lucasgber@gmail.com>
2023-12-17 11:34:45 -0300

    correções no package, index e .env para deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import { server } from './server/Server'
+import { Knex } from './server/database/knex';
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log('App rodando!');
-})
+
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log('App rodando!');
+  })
+}
+
+
+// Se estiver em modo de produção, roda a migration somente quando dá o deploy
+// Se estiver rodando no localhost, não precisa dar dando migration cada vez
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest()
+    .then(() => {
+      startServer()
+    })
+    .catch((err) => {console.log(err)})
+} else {
+  startServer();
+}
